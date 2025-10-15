41b8f4f6f3b76cc258eac23e4ee0ba63117ca0e1
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-15 23:57:08 +0530

    youtube clone

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Home.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Feed from "../../Components/Feed/Feed";
+import { useSearchParams } from "react-router-dom";
+
+const Home = ({ sidebar }) => {
+  const [category, setCategory] = useState(0);
+
+  return (
+    <>
+      <Sidebar
+        sidebar={sidebar}
+        category={category}
+        setCategory={setCategory}
+      />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed category={category} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
